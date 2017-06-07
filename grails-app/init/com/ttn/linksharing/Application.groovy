7c7b79b271b461a070068d2ae7eedef195788205
jitender saini <jitender.saini@tothenew.com>
2017-06-07 18:13:16 +0530

    locally running

--- grails-app/init/com/ttn/linksharing/Application.groovy
+++ grails-app/init/com/ttn/linksharing/Application.groovy
@@ -9,19 +9,19 @@ import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
 import org.springframework.context.EnvironmentAware
 
-class Application extends GrailsAutoConfiguration implements EnvironmentAware {
+class Application extends GrailsAutoConfiguration {  //implements EnvironmentAware
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
 
-    @Override
-    void setEnvironment(Environment environment) {
-        String configPath = System.properties["local.config.location"]
-        Resource resourceConfig = new FileSystemResource(configPath)
-        YamlPropertiesFactoryBean ypfb = new YamlPropertiesFactoryBean()
-        ypfb.setResources([resourceConfig] as Resource[])
-        ypfb.afterPropertiesSet()
-        Properties properties = ypfb.getObject()
-        environment.propertySources.addFirst(new PropertiesPropertySource("local.config.location", properties))
-    }
+//    @Override
+//    void setEnvironment(Environment environment) {
+//        String configPath = System.properties["local.config.location"]
+//        Resource resourceConfig = new FileSystemResource(configPath)
+//        YamlPropertiesFactoryBean ypfb = new YamlPropertiesFactoryBean()
+//        ypfb.setResources([resourceConfig] as Resource[])
+//        ypfb.afterPropertiesSet()
+//        Properties properties = ypfb.getObject()
+//        environment.propertySources.addFirst(new PropertiesPropertySource("local.config.location", properties))
+//    }
 }
