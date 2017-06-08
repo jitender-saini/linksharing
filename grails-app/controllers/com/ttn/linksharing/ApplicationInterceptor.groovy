7ab10fe3a02636b4f2fd23c707672cc9a389745c
jitender saini <jitender.saini@tothenew.com>
2017-06-08 10:34:17 +0530

    data source

--- grails-app/controllers/com/ttn/linksharing/ApplicationInterceptor.groovy
+++ grails-app/controllers/com/ttn/linksharing/ApplicationInterceptor.groovy
@@ -1,20 +1,21 @@
 package com.ttn.linksharing
 
 class ApplicationInterceptor {
-    ApplicationInterceptor() {
-        matchAll().excludes(controller: 'login')
-                .excludes (controller: 'user', action:'profile')
-                .excludes(controller: 'search', action: 'show')
-                .excludes(controller: 'topic', action: 'showTopic')
-                .excludes(controller: 'resource', action: 'show')
-    }
-
-    boolean before() {
-        if (!session['user']) {
-            redirect controller: "login", action: "index"
-        }
-        true
-    }
+//    ApplicationInterceptor() {
+//        matchAll().excludes(controller: 'login')
+//                .excludes (controller: 'user', action:'profile')
+//                .excludes(controller: 'search', action: 'show')
+//                .excludes(controller: 'topic', action: 'showTopic')
+//                .excludes(controller: 'resource', action: 'show')
+//                .excludes(controller: 'console')
+//    }
+//
+//    boolean before() {
+//        if (!session['user']) {
+//            redirect controller: "login", action: "index"
+//        }
+//        true
+//    }
 
     boolean after() {
         log.info "param: ${params}"
